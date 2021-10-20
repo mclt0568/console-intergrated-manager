d6922d6338bbc62d702671434f8c5a2d884a32ac
mclt0568 <tis7bfrankie@gmail.com>
2021-10-20 13:28:11 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_intergrated_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
